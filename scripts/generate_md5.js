dd75315ef450920a1b35b3f442dd28a1ca44a8d9
baldierot <baldierot@gmail.com>
2025-09-13 19:47:49 +0300

    generate md5 and deploy

--- scripts/generate_md5.js
+++ scripts/generate_md5.js
@@ -1,8 +1,10 @@
 
+const path = require('path');
 const crypto = require('crypto');
 const fs = require('fs');
 
 const generateMd5 = (filePath) => {
+  console.log(`Generating MD5 for: ${filePath}`);
   const fileBuffer = fs.readFileSync(filePath);
   const hash = crypto.createHash('md5');
   hash.update(fileBuffer);
@@ -11,10 +13,15 @@ const generateMd5 = (filePath) => {
 
 if (require.main === module) {
   const files = process.argv.slice(2);
+  console.log('Files to process:', files);
   files.forEach(file => {
     if (fs.existsSync(file) && !file.endsWith('.md5')) {
       const md5 = generateMd5(file);
-      fs.writeFileSync(`${file}.md5`, md5);
+      const newFile = path.join(path.dirname(file), `${path.parse(file).name}.md5`);
+      console.log(`Creating MD5 file: ${newFile}`);
+      fs.writeFileSync(newFile, md5);
+    } else {
+      console.log(`Skipping: ${file}`);
     }
   });
 }
