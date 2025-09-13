498f8370a97d8c44e4598c474938a1ddf0df5a10
baldierot <baldierot@gmail.com>
2025-09-13 18:14:26 +0300

    0

--- scripts/pre-commit.js
+++ scripts/pre-commit.js
@@ -1,12 +1,13 @@
 
 const { execSync } = require('child_process');
 
-const stagedFiles = execSync('git diff --cached --name-only --diff-filter=A').toString().split('\n');
+const stagedFiles = execSync('git diff --cached --name-only --diff-filter=A').toString().split('\n').filter(file => file);
 
 stagedFiles.forEach(file => {
   if (file.endsWith('.pck')) {
     const newFile = file.slice(0, -4);
     execSync(`mv "${file}" "${newFile}"`);
     execSync(`git add "${newFile}"`);
+    execSync(`git reset -- "${file}"`);
   }
 });
